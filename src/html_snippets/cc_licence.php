0d9b6ad5a76571321d499122ddfea24fc9a8aba6
Tobias Wenzel <tobiasw225@gmail.com>
2018-08-13 11:21:47 +0200

    html-snippets

---
+++ src/html_snippets/cc_licence.php
@@ -0,0 +1,5 @@
+
+
+<div class="navbar navbar-fixed-bottom">
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
+</div>
--- src/index.php
+++ src/index.php
@@ -1,85 +1,35 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
-		<title>music matcher</title>
-		<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  		<link rel="stylesheet" href="css/bootstrap.min.css" type = "text/css"/>
-
-		<!-- CropSelectJs files -->
-		<link href="node_modules/crop-select-js/crop-select-js.min.css" rel="stylesheet" type="text/css" />
-		<script src="node_modules/crop-select-js//crop-select-js.min.js"></script>
-		
-		<!-- Tooltips --> 
-   		<link rel="stylesheet" type="text/css" href="css/tooltipster.bundle.min.css" />
-    	<script type="text/javascript" src="js/tooltipster.bundle.min.js"></script>
-    	
-    	<!-- Custom javascript -->
-		<script src="js/script.js" type="text/javascript"></script>
-
-		<!-- Not required, used for page layout -->
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-		<!-- Custom styles -->
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<?php
+		include ('html_snippets/header.php');
+		?>
 	</head>
-	
 
 	<body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">music matcher</a>
-        <!-- responsive menu -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link" href="mark_image.php">detect</a>
-            </li>
+		<?php
+		include ('html_snippets/nav_bar.php');
+		?>
 
+		<div class="container" style="margin-top: 10px">
+			<h1>music matcher</h1>
+			<h2>How to get started</h2>
+			<p>
+				...
+			</p>
 
-            <li class="nav-item">
-              <a class="nav-link" href="crop_notes.php">cropper</a>
-            </li>
-                        <li class="nav-item">
-              <a class="nav-link" href="note_correction.php">note correction</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">contact</a>
-            </li>
+			<h2>Contribute</h2>
+			<p>
+				...
+			</p>
+			<a href="https://github.com/tobiasw225/musicmatcher">on github</a>
 
-            <li class="nav-item">
-              <a class="nav-link" href="login.php">sign in</a>
-            </li>
-            
-          </ul>
-        </div>
-      </div>
- 
-    </nav>
-    		<div class="container" style="margin-top: 10px">
-	<h1>music matcher</h1>
-<h2>How to get started</h2>
-<p>...</p>	
+		</div>
 
-<h2>Contribute</h2>
-<p>...</p>	
-<a href="https://github.com/tobiasw225/musicmatcher">on github</a>
-
-
-</div>
-<div class="navbar navbar-fixed-bottom">
-<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
-</div>
+		<?php
+		Include ('html_snippets/cc_licence.php');
+		?>
 	</body>
 </html>
