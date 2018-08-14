4e6ba352e86d8c135d8b19afed7fd5837b919e33
Tobias Wenzel <tobiasw225@gmail.com>
2018-08-14 17:10:49 +0200

    neues docker-compose file

--- docker/build.sh
+++ docker/build.sh
@@ -4,50 +4,8 @@
 
 echo "...........................................";
 echo "Welcome to our preparation script.";
-echo "We simply check whether audiveris has already been cloned and do if necessary.";
 printf "...........................................\n\n";
 
-# set project directories
-project_dir="$(dirname "$(pwd)")"
-audiveris_dir="backend/audiveris";
-
-audiveris_in_folder=false;
-
-# check whether audiveris is already present
-if [ -d "$audiveris_dir" ]; then
-
-# check whether audiveris is complete
-folder_size="`du -hs $audiveris_dir`";
-folder_size=${folder_size%M*};
-audiveris_size=300; #actually 398 M
-
-
-if [ "$folder_size" -ge "$audiveris_size" ]; then
-  echo "$audiveris_dir seems ok";
-  audiveris_in_folder=true;
-else
-  echo "$audiveris_dir seems not ok. I'm going to remove it and reclone.";
-  audiveris_in_folder=false;
-fi
-else
-  audiveris_in_folder=false;
-fi
-
-
-if [ "$audiveris_in_folder"=true ]; then
-  echo "audiveris is already in the correct folder.";
-  echo "if there's a new version of audiveris you might have to clone it.";
-else
-  echo "i'm going to clone a fresh version of audiveris.";
-  git clone https://github.com/Audiveris/audiveris.git 
-  cd ../backend/audiveris && \
-  git checkout development
-fi
-
-
-
-echo 'finished audiveris part';
-printf "...........................................\n\n";
 
 echo '...docker part';
 
