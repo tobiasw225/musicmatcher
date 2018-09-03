537c99b873ca3d6de6c6ef753e99d8e2b91e0038
pasha <brodianskyi@gmail.com>
2018-09-03 15:21:29 +0200

    add delete_attribute function

--- src/js/add_image.js
+++ src/js/add_image.js
@@ -6,14 +6,17 @@
 
 });*/
 
+
 function save_content() {
-    console.log("Zaschli!!!!!!!!!!");
+    delete_attr();
     var a = document.body.appendChild(document.createElement("a"));
     a.download = "export.html";
     a.href = "data:text/html," + document.getElementById("sidebar").innerHTML;
     a.click();
-}
 
+
+
+}
 
 
 function edit_content() {
@@ -27,26 +30,39 @@ function add_attr() {
     console.log("in add_attr function");
     var ocr_line_elements = document.getElementsByClassName("ocr_line");
     console.log(ocr_line_elements.length);
+
     for (var i = 0; i < ocr_line_elements.length; i++) {
-        console.log("in loop add_attr");
-        console.log(ocr_line_elements[i]);
+        console.log("in loop true");
         ocr_line_elements[i].setAttribute("onmouseover", "mouse_over(this)");
         ocr_line_elements[i].setAttribute("onmouseout", "mouse_out(this)");
 
-    }
 
+    }
+}
+
+function delete_attr(){
+    var ocr_line_elements = document.getElementsByClassName("ocr_line");
+    for (var i = 0; i < ocr_line_elements.length; i++) {
+        console.log("in loop false");
+        ocr_line_elements[i].removeAttribute("onmouseover");
+        ocr_line_elements[i].removeAttribute("onmouseout");
+        ocr_line_elements[i].setAttribute("contenteditable", "false");
+ //        ocr_line_elements[i].style.backgroundColor = "White";
+
+    }
 
 }
 
+
 function mouse_over(element) {
-  console.log("in mouse_over");
-  element.setAttribute("contenteditable","true");
-  element.style.backgroundColor = "lightblue";
+    console.log("in mouse_over");
+    element.setAttribute("contenteditable", "true");
+    element.style.backgroundColor = "lightblue";
 }
 
 
 function mouse_out(element) {
-  console.log("in mouse out");
+    console.log("in mouse out");
     element.style.backgroundColor = "#ECF5E4";
 
 }
