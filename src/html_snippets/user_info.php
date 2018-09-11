8aec384d9b0f6a79bbb638a4f1037641472925e5
Unknown <tobiasw225@gmail.com>
2018-09-11 15:08:00 +0200

    user infos in nav bar

--- src/html_snippets/user_info.php
+++ src/html_snippets/user_info.php
@@ -1,14 +1,13 @@
-			<div class='user_info'>
+			<li class='user_info'>
 			<?php
 				if (isset($_SESSION['user_name'])) {
 					$user_name = $_SESSION['user_name'];
 					$user_id = $_SESSION['user_id'];
 					$user_points = $_SESSION['user_points'];
-					echo "<span class='glyphicon glyphicon-user'></span> <span class='current_user' u_id=$user_id>$user_name</span><br/>";
-					
-					echo "<span class='glyphicon glyphicon-music'></span>  <span class='current_user' u_id=$user_id>$user_points</span>";
-				} else {
+					echo "<li><a href='#'><span class='glyphicon glyphicon-user' u_id=$user_id></span> $user_name
+					| <span class='glyphicon glyphicon-music'></span> <span u_id=$user_id>$user_points</span></a></li>";
+					} else {
 					echo 'not logged in';
 				}
 			?>
-			</div>
+			</li>
